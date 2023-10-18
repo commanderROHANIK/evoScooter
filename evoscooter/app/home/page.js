5d218750ba90e2c59b67a6ebba93cd07b31f4d38
Balázs Kerekes <kerekes.balazs.12@gmail.com>
2023-10-18 21:16:00 +0200

    add dummy home page

---
+++ evoscooter/app/home/page.js
@@ -0,0 +1,25 @@
+export default async function Login() {
+  const todos = await getStaticProps();
+
+  console.log(todos);
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 background">
+        {
+            todos.map((todo) => {
+                return (
+                    <div 
+                        key={todo.id}
+                        className="listItem">
+                        <p>{todo.title}</p>
+                    </div>
+                )
+            })
+        }
+    </div>
+  )
+}
+
+async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    return response.json();
+}
