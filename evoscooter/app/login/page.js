8d631ec914f70316c01e9444efb50c3a691832eb
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2023-11-04 14:14:08 +0100

    fix button colors on login and registration page

--- evoscooter/app/login/page.js
+++ evoscooter/app/login/page.js
@@ -61,7 +61,7 @@ export default function Login() {
         <div className="m-6">
           <button
             type="button"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             onClick={handleLogin}
           >
             Sign in
@@ -70,7 +70,7 @@ export default function Login() {
         <div className="m-6">
           <Link
             href="/registration"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            className="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Registrate
           </Link>
         </div>
