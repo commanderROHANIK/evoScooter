38c4f41587e61317f1a71bdb06165f423c857c12
Balázs Kerekes <kerekes.balazs.12@gmail.com>
2023-11-09 13:26:42 +0100

    update database

--- db/evoscooter.sql
+++ db/evoscooter.sql
@@ -1,160 +1,142 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 13, 2023 at 10:54 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: evoscooter
+-- ------------------------------------------------------
+-- Server version	11.1.2-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `evoscooter`
---
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `rentals`
 --
 
+DROP TABLE IF EXISTS `rentals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rentals` (
   `User.Email` varchar(100) NOT NULL,
   `Vehicle.Id` int(11) NOT NULL,
   `StartTime` datetime NOT NULL,
   `EndTime` datetime NOT NULL,
-  `State` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+  `State` varchar(100) NOT NULL,
+  PRIMARY KEY (`User.Email`,`Vehicle.Id`),
+  KEY `VehicleKey` (`Vehicle.Id`),
+  CONSTRAINT `UserKey` FOREIGN KEY (`User.Email`) REFERENCES `user` (`Email`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `VehicleKey` FOREIGN KEY (`Vehicle.Id`) REFERENCES `vehicle` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `rentals`
+--
+
+LOCK TABLES `rentals` WRITE;
+/*!40000 ALTER TABLE `rentals` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rentals` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `site`
 --
 
+DROP TABLE IF EXISTS `site`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `site` (
-  `Address` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+  `Address` varchar(100) NOT NULL,
+  UNIQUE KEY `Address` (`Address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `site`
 --
 
-INSERT INTO `site` (`Address`) VALUES
-('Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),
-('Miskolc, Arany János tér 1, 3526 Magyarország'),
-('Szeged, Horváth Mihály u. 5, 6720 Magyarország');
-
--- --------------------------------------------------------
+LOCK TABLES `site` WRITE;
+/*!40000 ALTER TABLE `site` DISABLE KEYS */;
+INSERT INTO `site` VALUES ('Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),('Miskolc, Arany János tér 1, 3526 Magyarország'),('Szeged, Horváth Mihály u. 5, 6720 Magyarország');
+/*!40000 ALTER TABLE `site` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `Email` varchar(100) NOT NULL,
   `Name` varchar(250) NOT NULL,
-  `LicenseNumber` varchar(100) NOT NULL,
+  `LicenseNumber` varchar(100) DEFAULT NULL,
   `Type` varchar(10) NOT NULL,
-  `SiteAddress` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+  `SiteAddress` varchar(100) DEFAULT 'Szeged, Horváth Mihály u. 5, 6720 Magyarország',
+  `Password` varchar(100) NOT NULL,
+  PRIMARY KEY (`Email`),
+  UNIQUE KEY `Email` (`Email`),
+  KEY `SiteKey` (`SiteAddress`),
+  CONSTRAINT `SiteKey` FOREIGN KEY (`SiteAddress`) REFERENCES `site` (`Address`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`Email`, `Name`, `LicenseNumber`, `Type`, `Site.Address`) VALUES
-('mindta@evosoft.com', 'Minta Bela', '3434KF', 'USR', 'Szeged, Horváth Mihály u. 5, 6720 Magyarország');
-
--- --------------------------------------------------------
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('nutow@mailinator.com','Eaton Gamble',NULL,'usr','Szeged, Horváth Mihály u. 5, 6720 Magyarország','Pa$$w0rd!');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `vehicle`
 --
 
+DROP TABLE IF EXISTS `vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `vehicle` (
-  `ID` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Type` varchar(100) NOT NULL,
   `Rentable` tinyint(1) NOT NULL,
-  `Site.Address` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+  `Site.Address` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID` (`ID`),
+  KEY `Site.Address` (`Site.Address`),
+  CONSTRAINT `OtherSiteKey` FOREIGN KEY (`Site.Address`) REFERENCES `site` (`Address`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `vehicle`
 --
 
---
--- Indexes for table `rentals`
---
-ALTER TABLE `rentals`
-  ADD PRIMARY KEY (`User.Email`,`Vehicle.Id`),
-  ADD KEY `VehicleKey` (`Vehicle.Id`);
+LOCK TABLES `vehicle` WRITE;
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for table `site`
+-- Dumping routines for database 'evoscooter'
 --
-ALTER TABLE `site`
-  ADD UNIQUE KEY `Address` (`Address`);
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`Email`),
-  ADD UNIQUE KEY `Email` (`Email`),
-  ADD KEY `SiteKey` (`SiteAddress`);
-
---
--- Indexes for table `vehicle`
---
-ALTER TABLE `vehicle`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `ID` (`ID`),
-  ADD KEY `Site.Address` (`Site.Address`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `vehicle`
---
-ALTER TABLE `vehicle`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `rentals`
---
-ALTER TABLE `rentals`
-  ADD CONSTRAINT `UserKey` FOREIGN KEY (`User.Email`) REFERENCES `user` (`Email`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `VehicleKey` FOREIGN KEY (`Vehicle.Id`) REFERENCES `vehicle` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `SiteKey` FOREIGN KEY (`SiteAddress`) REFERENCES `site` (`Address`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `vehicle`
---
-ALTER TABLE `vehicle`
-  ADD CONSTRAINT `OtherSiteKey` FOREIGN KEY (`Site.Address`) REFERENCES `site` (`Address`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-09 13:24:03
