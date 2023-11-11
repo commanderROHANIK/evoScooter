0c9898444905f4acd12f84aca8476606fdd2774b
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2023-11-11 15:00:27 +0100

    make home page protected

--- evoscooter/app/home/page.js
+++ evoscooter/app/home/page.js
@@ -1,30 +1,38 @@
 import Header from "../commonComponents/header";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Login() {
-  const todos = await getStaticProps();
+    const vehicles = await getStaticProps();
+    const session = await getServerSession(authOptions);
 
-  return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8" >
-        <Header />        
-        {
-            todos.map((todo) => {
-                return (
-                        <div 
-                            key={todo.id}
+    if (!session) {
+        redirect("/login");
+    }
+
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8" >
+            <Header />
+            {
+                vehicles.map((vehicle) => {
+                    return (
+                        <div
+                            key={vehicle.id}
                             className="bg-white m-5 text-black rounded-xl h-16">
-                            <p>{todo.Type}</p>
+                            <p>{vehicle.Type}</p>
                         </div>
-                )
-            })
-        }
-    </div>
-  )
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 async function getStaticProps() {
     // const response = await fetch("/api/vehicles");
     const response = await fetch('http://localhost:3000/api/vehicles', {
         method: 'GET',
-      });
+    });
     return response.json();
 }
