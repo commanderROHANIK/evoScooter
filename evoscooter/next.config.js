66fc54667d6056f630bab1b392f69768751f823c
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2023-11-11 17:24:13 +0100

    move to typescript

--- evoscooter/next.config.js
+++ evoscooter/next.config.js
@@ -1,11 +1,4 @@
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/login',
-          permanent: true,
-        },
-      ]
-    },
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
