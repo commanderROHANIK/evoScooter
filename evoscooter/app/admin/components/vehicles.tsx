9963b3c043c170c481715efd102bcc038d09ac90
Balázs Kerekes <kerekes.balazs.12@gmail.com>
2023-12-29 20:04:07 +0100

    design for the stuff for vehicles on admin page

--- evoscooter/app/admin/components/vehicles.tsx
+++ evoscooter/app/admin/components/vehicles.tsx
@@ -1,24 +1,24 @@
 "use client";
 
 import { useState } from "react";
-// import { getVehicles } from "../actions";
 import AddVehicle from "./AddVehicle";
 import CustomButton from "./CustomButton";
 import { VehicleData, VehicleProps } from "@/types";
 
 
 export default function Vehicles(AllVehicles: VehicleProps) {
-    // const vehicles = await getVehicles();
     const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <h2>Vehicles</h2>
-            <CustomButton
-                title='Add'
-                containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
-                textStyles='text-white text-[14px] leading-[17px] font-bold'
-                handleClick={() => setIsOpen(true)}>
-            </CustomButton>
+            <div className="flex">
+                <p className="text-lg font-medium leading-6 text-gray-900 py-2 px-4 rounded text-white px-12">Vehicles</p>
+                <CustomButton
+                    title='Add'
+                    containerStyles='justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 ml-4'
+                    textStyles='text-white text-[14px] leading-[17px] font-bold'
+                    handleClick={() => setIsOpen(true)}>
+                </CustomButton>
+            </div>
             <AddVehicle isOpen={isOpen} closeModal={() => setIsOpen(false)} />
             {
                 AllVehicles.Vehicles.map((vehicle: VehicleData) => {
