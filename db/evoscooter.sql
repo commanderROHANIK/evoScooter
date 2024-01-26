5992ccc52736984cbc624681a8befe1bfff0004a
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2024-01-26 11:48:02 +0100

    update sql

--- db/evoscooter.sql
+++ db/evoscooter.sql
@@ -23,15 +23,15 @@ DROP TABLE IF EXISTS `rentals`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rentals` (
-  `User.Email` varchar(100) NOT NULL,
-  `Vehicle.Id` int(11) NOT NULL,
-  `StartTime` datetime NOT NULL,
-  `EndTime` datetime NOT NULL,
+  `UserEmail` varchar(100) NOT NULL,
+  `VehicleId` int(11) NOT NULL,
+  `StartTime` datetime NOT NULL DEFAULT '2023-01-01 00:00:00',
+  `EndTime` datetime NOT NULL DEFAULT '2023-01-02 00:00:00',
   `State` varchar(100) NOT NULL,
-  PRIMARY KEY (`User.Email`,`Vehicle.Id`),
-  KEY `VehicleKey` (`Vehicle.Id`),
-  CONSTRAINT `UserKey` FOREIGN KEY (`User.Email`) REFERENCES `user` (`Email`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `VehicleKey` FOREIGN KEY (`Vehicle.Id`) REFERENCES `vehicle` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`UserEmail`,`VehicleId`, `StartTime`), 
+  KEY `VehicleKey` (`VehicleId`),
+  CONSTRAINT `UserKey` FOREIGN KEY (`UserEmail`) REFERENCES `user` (`Email`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `VehicleKey` FOREIGN KEY (`VehicleId`) REFERENCES `vehicle` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -41,6 +41,7 @@ CREATE TABLE `rentals` (
 
 LOCK TABLES `rentals` WRITE;
 /*!40000 ALTER TABLE `rentals` DISABLE KEYS */;
+INSERT INTO `rentals` VALUES ('admin@admin.com',1,'2024-01-24 21:15:00','2024-01-31 21:15:00','Approved'),('admin@admin.com',2,'2024-01-25 12:20:00','2024-01-29 12:20:00','Declined'),('admin@admin.com',4,'2024-01-26 13:00:00','2024-01-31 13:00:00','Declined');
 /*!40000 ALTER TABLE `rentals` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +95,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('nutow@mailinator.com','Eaton Gamble',NULL,'usr','Szeged, Horváth Mihály u. 5, 6720 Magyarország','Pa$$w0rd!');
+INSERT INTO `user` VALUES ('admin@admin.com','Tam Da Dam',NULL,'usr','Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország','$2b$10$pMvC.ntPo269.eD.MviVHOrBUXwpspn2KY9rSQ65epzoMzKJnAZY6'),('user1@example.com','John Smith',NULL,'usr','Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország','$2b$10$pMvC.ntPo269.eD.MviVHOrBUXwpspn2KY9rSQ65epzoMzKJnAZY6'),('user4@example.com','Emily Davis',NULL,'usr','Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország','$2b$10$pMvC.ntPo269.eD.MviVHOrBUXwpspn2KY9rSQ65epzoMzKJnAZY6'),('user5@example.com','Michael Brown',NULL,'usr','Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország','$2b$10$pMvC.ntPo269.eD.MviVHOrBUXwpspn2KY9rSQ65epzoMzKJnAZY6'),('user8@example.com','Olivia Garcia',NULL,'usr','Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország','$2b$10$pMvC.ntPo269.eD.MviVHOrBUXwpspn2KY9rSQ65epzoMzKJnAZY6');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,13 +109,13 @@ DROP TABLE IF EXISTS `vehicle`;
 CREATE TABLE `vehicle` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Type` varchar(100) NOT NULL,
-  `Rentable` tinyint(1) NOT NULL,
+  `Rentable` tinyint(4) NOT NULL,
   `Site.Address` varchar(100) NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE KEY `Id` (`Id`),
+  UNIQUE KEY `ID` (`Id`),
   KEY `Site.Address` (`Site.Address`),
   CONSTRAINT `OtherSiteKey` FOREIGN KEY (`Site.Address`) REFERENCES `site` (`Address`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,6 +124,7 @@ CREATE TABLE `vehicle` (
 
 LOCK TABLES `vehicle` WRITE;
 /*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+INSERT INTO `vehicle` VALUES (1,'Electric Scooter Model A',0,'Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),(2,'Electric Scooter Model B',1,'Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),(3,'Electric Scooter Model C',1,'Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),(4,'Electric Scooter Model D',1,'Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),(6,'Electric Scooter Model F',1,'Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),(7,'Electric Scooter Model G',1,'Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),(8,'Electric Scooter Model H',1,'Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),(9,'Electric Scooter Model I',1,'Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország'),(10,'Electric Scooter Model J',1,'Budapest, Magyar Tudósok Körútja 11, 1117 Magyarország');
 /*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-09 13:24:03
+-- Dump completed on 2024-01-26 11:43:28
