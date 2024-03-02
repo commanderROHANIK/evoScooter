fc11cc807a8bc8f6471bf8ad09814b4268d93e39
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2024-03-02 19:45:07 +0100

    add vehicle list component, add it to home page

--- app/evoscooter/src/app/home/home.component.ts
+++ app/evoscooter/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../commonComponents/header/header.component';
+import { VehicleListComponent } from "../commonComponents/vehicle-list/vehicle-list.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, VehicleListComponent]
 })
 export class HomeComponent {
 
