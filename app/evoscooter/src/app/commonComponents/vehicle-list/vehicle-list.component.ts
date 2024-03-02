321e01bca0a5e1969ec738dfe32f3fd7f54b9abf
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2024-03-02 20:06:05 +0100

    create vehicle card for vehicle list, make the first version, only with built in values. service will be needed

--- app/evoscooter/src/app/commonComponents/vehicle-list/vehicle-list.component.ts
+++ app/evoscooter/src/app/commonComponents/vehicle-list/vehicle-list.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { VehicleCardComponent } from './vehicle-card/vehicle-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vehicle-list',
   standalone: true,
-  imports: [],
+  imports: [VehicleCardComponent, CommonModule],
   templateUrl: './vehicle-list.component.html',
   styleUrl: './vehicle-list.component.scss'
 })
 export class VehicleListComponent {
-
+  vehicleList: { name: string, value: string }[] = [
+    { name: 'Object 1', value: 'Value 1' },
+    { name: 'Object 2', value: 'Value 2' },
+    { name: 'Object 3', value: 'Value 3' },
+    { name: 'Object 4', value: 'Value 4' },
+    { name: 'Object 5', value: 'Value 5' }
+  ];
 }
