aa29115ec680aff9dbb09ebe5565df7e28563ecd
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2024-03-02 21:27:53 +0100

    start inplementing add vehicle functionality, create basics of it

--- app/evoscooter/src/app/admin/admin.component.ts
+++ app/evoscooter/src/app/admin/admin.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../commonComponents/header/header.component";
+import { VehicleListComponent } from "../commonComponents/vehicle-list/vehicle-list.component";
 
 @Component({
     selector: 'app-admin',
     standalone: true,
     templateUrl: './admin.component.html',
     styleUrl: './admin.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, VehicleListComponent]
 })
 export class AdminComponent {
 
--- app/evoscooter/src/app/commonComponents/vehicle-list/vehicle-list.component.ts
+++ app/evoscooter/src/app/commonComponents/vehicle-list/vehicle-list.component.ts
@@ -1,21 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { VehicleCardComponent } from './vehicle-card/vehicle-card.component';
 import { CommonModule } from '@angular/common';
 import { VehicleService } from '../../services/vehicle.service';
+import { AddVehicleComponent } from "./add-vehicle/add-vehicle.component";
 
 @Component({
-  selector: 'app-vehicle-list',
-  standalone: true,
-  imports: [VehicleCardComponent, CommonModule],
-  templateUrl: './vehicle-list.component.html',
-  styleUrl: './vehicle-list.component.scss'
+    selector: 'app-vehicle-list',
+    standalone: true,
+    templateUrl: './vehicle-list.component.html',
+    styleUrl: './vehicle-list.component.scss',
+    imports: [VehicleCardComponent, CommonModule, AddVehicleComponent]
 })
 
 export class VehicleListComponent {
+  @Input() showTitle!: boolean;
+
   vehicleList: { name: string; value: string; }[] | undefined;
   vehicleService: VehicleService = inject(VehicleService);
+  popupVisible: boolean = false;
 
   constructor() {
     this.vehicleList = this.vehicleService.getAllVehicles();
+  }
+
+  addVehicle() {
+    this.popupVisible = !this.popupVisible;
   }
 }
