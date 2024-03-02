3b41c63d8bb88619d47fc4e4dbf1c36c3b9fec14
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2024-03-02 10:49:29 +0100

    make login and registration skeltons

--- app/evoscooter/src/app/login/login.component.ts
+++ app/evoscooter/src/app/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule, } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+onFormSubmit($event: SubmitEvent) {
+  $event.preventDefault();
 }
+  emailFormControl!: FormControl<any>;
+  passwordFormControl!: FormControl<any>;
+}
+
--- app/evoscooter/src/app/registration/registration.component.ts
+++ app/evoscooter/src/app/registration/registration.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+  emailFormControl!: FormControl<any>;
+  passwordFormControl!: FormControl<any>;
+  passwordAgainFormControl!: FormControl<any>;
+  siteFomrControl!: FormControl<any>;
 
+  onFormSubmit($event: SubmitEvent) {
+    $event.preventDefault();
+  }
 }
