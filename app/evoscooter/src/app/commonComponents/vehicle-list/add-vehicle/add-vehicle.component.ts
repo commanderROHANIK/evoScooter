7d26116862db1a15bb533f39627e88981e6221c4
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2024-03-02 22:03:28 +0100

    create prototype of add vehicle popup

--- app/evoscooter/src/app/commonComponents/vehicle-list/add-vehicle/add-vehicle.component.ts
+++ app/evoscooter/src/app/commonComponents/vehicle-list/add-vehicle/add-vehicle.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule, } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-vehicle',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './add-vehicle.component.html',
   styleUrl: './add-vehicle.component.scss'
 })
 export class AddVehicleComponent {
-
+  idFormControl!: FormControl<any>;
+  typeFormControl!: FormControl<any>;
+  siteFormControl!: FormControl<any>;
 }
