5db2a28588847bba884708ae1802da6eb4b1a3a4
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2024-03-22 14:55:50 +0100

    add docker file and make changes required for running from container

--- evoscooter/next.config.js
+++ evoscooter/next.config.js
@@ -8,7 +8,10 @@ const nextConfig = {
                 permanent: true
             }
         ];
-    }
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 }
 
 module.exports = nextConfig
