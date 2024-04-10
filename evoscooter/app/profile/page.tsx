204157dbcda797f8edd8b9ed3bc0f3cfdce78408
Balázs Kerekes <kerekes.balazs.12@gmail.com>
2024-04-10 21:10:17 +0200

    devide the main part of profile page

--- evoscooter/app/profile/page.tsx
+++ evoscooter/app/profile/page.tsx
@@ -5,8 +5,13 @@ export default async function Login() {
     <div className="flex min-h-screen">
       <div className="flex flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <Header />
-        <div className="flex bg-white flex-1 flex-col justify-center px-6 py-12 lg:px-8 mt-6 rounded-xl">
-          
+        <div className="bg-white flex-1 mt-6 rounded-xl">
+          <span className="bg-black w-1/5 flex-1 inline-flex rounded-tl-xl">
+            tmp
+          </span>
+          <span className="bg-black w-4/5 flex-1 inline-flex rounded-tr-xl">
+            tmp2
+          </span>
         </div>
       </div>
     </div>
